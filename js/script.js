5b203a5bfc04a7e8d3d361d5fb58b4bc84600f52
uiuxkimabe <uiuxkimabe@gmail.com>
2023-01-18 12:07:27 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamBtn = document.querySelector('.hamBtn');
+const navigasi = document.querySelector('.navigasi');
+hamBtn.addEventListener('click', ()=> {
+   navigasi.classList.toggle('show')
+})
+
